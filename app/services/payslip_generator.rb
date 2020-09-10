3ed3358e9d596bc40b8c0ae88efa16105b56cf63
aimanyusra <aimanyusra@hotmail.com>
2020-09-11 00:47:31 +0800

    Init payslip_generator service object

---
+++ app/services/payslip_generator.rb
@@ -0,0 +1,12 @@
+class PayslipGenerator
+	attr_reader :employee_name, :annual_salary
+
+  def initialize(employee_name, annual_salary)
+  	@employee_name = employee_name
+  	@annual_salary = annual_salary
+  end
+
+  def generate
+
+  end
+end
