9d3e4c04ddfe776cd4e1e01a6ba5e67072c968b5
aimanyusra <aimanyusra@hotmail.com>
2020-09-13 00:54:09 +0800

    Generate SalaryRecord model

--- app/controllers/api/payslip_generator_controller.rb
+++ app/controllers/api/payslip_generator_controller.rb
@@ -1,6 +1,6 @@
 class Api::PayslipGeneratorController < ApiController
 	def create
-
-		render json: PayslipGenerator.new(params[:employee_name], params[:employee_salary]).generate
+		render json: PayslipGenerator.new(params[:employee_name], params[:employee_salary]).generate, status: 200
+		byebug
 	end
 end
