c8ef658c68b40ad34cb2077533ae60c60daec0eb
aimanyusra <aimanyusra@hotmail.com>
2020-09-13 01:36:47 +0800

    Finish get records

--- app/controllers/api/payslip_generator_controller.rb
+++ app/controllers/api/payslip_generator_controller.rb
@@ -1,4 +1,21 @@
 class Api::PayslipGeneratorController < ApiController
+	def index
+		salary_computations = []
+		SalaryRecord.all.each do |record|
+			obj = {
+				time_stamp: record.created_at.to_i,
+				employee_name: record.employee_name,
+				annual_salary: record.annual_salary,
+				monthly_income_tax: record.monthly_income_tax
+			}
+
+			salary_computations << obj
+		end
+
+		render json: { salary_computations: salary_computations }
+	end
+
+
 	def create
 		render json: PayslipGenerator.new(params[:employee_name], params[:employee_salary]).generate, status: 200
 
