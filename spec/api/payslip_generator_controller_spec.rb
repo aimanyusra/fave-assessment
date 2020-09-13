03a98e2fe78d3fe71a1c9e9bbc6760891a2a10a4
aimanyusra <aimanyusra@hotmail.com>
2020-09-13 16:32:25 +0800

    Remove byebug

--- spec/api/payslip_generator_controller_spec.rb
+++ spec/api/payslip_generator_controller_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe Api::PayslipGeneratorController do
     end
     it "JSON body response contains expected recipe attributes" do
       res = JSON.parse(response.body)
-      byebug
       expect(res.keys).to match_array(["salary_computations"])
     end
   end
