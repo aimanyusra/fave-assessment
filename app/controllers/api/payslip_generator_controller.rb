30df683184be662f91d95ca5749ac1645c001796
aimanyusra <aimanyusra@hotmail.com>
2020-09-14 14:38:05 +0800

    Use restful naming for payslip generate

--- app/controllers/api/payslip_generator_controller.rb
+++ app/controllers/api/payslip_generator_controller.rb
@@ -1,6 +1,29 @@
 class Api::PayslipGeneratorController < ApiController
-	def create
+	def index
+		salary_computations = []
+		SalaryRecord.all.each do |record|
+			obj = {
+				time_stamp: record.created_at.to_i.to_s,
+				employee_name: record.employee_name,
+				annual_salary: record.annual_salary,
+				monthly_income_tax: record.monthly_income_tax
+			}
 
-		render json: PayslipGenerator.new(params[:employee_name], params[:employee_salary]).generate
+			salary_computations << obj
+		end
+
+		render json: { salary_computations: salary_computations }
+	end
+
+
+	def create
+		payslip = PayslipGenerator.new(params[:employee_name], params[:employee_salary]).generate
+		if payslip
+			body = JSON.parse response.body
+			SalaryRecord.create(employee_name: params[:employee_name], annual_salary: params[:employee_salary], monthly_income_tax: body["monthly_income_tax"])
+			render json: payslip, status: 200
+		else
+			render json: error, status: 500
+		end
 	end
 end
