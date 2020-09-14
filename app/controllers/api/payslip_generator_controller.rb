dc1a9d3fe3663b14f8c0c86b3335c5354f647f69
aimanyusra <aimanyusra@hotmail.com>
2020-09-14 15:53:49 +0800

    Make generate method more readable

--- app/controllers/api/payslip_generator_controller.rb
+++ app/controllers/api/payslip_generator_controller.rb
@@ -1,10 +1,11 @@
 class Api::PayslipGeneratorController < ApiController
 	def generate
-		render json: PayslipGenerator.new(params[:employee_name], params[:employee_salary]).generate
-
-		if status == 200
+		if payslip = PayslipGenerator.new(params[:employee_name], params[:employee_salary]).generate
+			render json: payslip, status: 200
 			body = JSON.parse response.body
 			SalaryRecord.create(employee_name: params[:employee_name], annual_salary: params[:employee_salary], monthly_income_tax: body["monthly_income_tax"])
+		else
+			render json: error, status: 500
 		end
 	end
 end
