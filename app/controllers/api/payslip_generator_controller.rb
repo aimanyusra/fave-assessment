e127a27f81fab376e362e88b59822d7414916508
aimanyusra <aimanyusra@hotmail.com>
2020-09-14 15:56:14 +0800

    Refactor code to make it more readable

--- app/controllers/api/payslip_generator_controller.rb
+++ app/controllers/api/payslip_generator_controller.rb
@@ -16,12 +16,11 @@ class Api::PayslipGeneratorController < ApiController
 	end
 
 
-	def create
-		payslip = PayslipGenerator.new(params[:employee_name], params[:employee_salary]).generate
-		if payslip
+	def generate
+		if payslip = PayslipGenerator.new(params[:employee_name], params[:employee_salary]).generate
+			render json: payslip, status: 200
 			body = JSON.parse response.body
 			SalaryRecord.create(employee_name: params[:employee_name], annual_salary: params[:employee_salary], monthly_income_tax: body["monthly_income_tax"])
-			render json: payslip, status: 200
 		else
 			render json: error, status: 500
 		end
